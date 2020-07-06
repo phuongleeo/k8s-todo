47342f62de7d9ef80fa73dcf7825bffa7d6595ae
phuong-leeo <p.le@ryte.com>
2020-07-07 00:21:15 +0700

    setup EKS and helm monitoring

--- tf-INF/development/eks/k8s.tf
+++ tf-INF/development/eks/k8s.tf
@@ -1,3 +1,11 @@
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.production.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.production.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.production.token
+  load_config_file       = false
+  version                = "~> 1.9"
+}
+
 resource "kubernetes_cluster_role" "admin" {
   metadata {
     name = "admin-role"
@@ -40,5 +48,24 @@ resource "kubernetes_role_binding" "admin" {
     kind      = "ServiceAccount"
     name      = "admin"
     namespace = "kube-system"
+  }
+}
+
+resource "kubernetes_namespace" "bootstrap" {
+  metadata {
+    labels = {
+      stack = local.namespace[0]
+    }
+
+    name = local.namespace[0]
+  }
+}
+resource "kubernetes_namespace" "monitoring" {
+  metadata {
+    labels = {
+      stack = local.namespace[1]
+    }
+
+    name = local.namespace[1]
   }
 }
