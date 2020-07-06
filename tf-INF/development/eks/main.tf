159d64643ea4ddfc3a96b820870b0deb1a3851cf
phuong-leeo <p.le@ryte.com>
2020-07-07 00:21:15 +0700

    setup EKS and helm monitoring

--- tf-INF/development/eks/main.tf
+++ tf-INF/development/eks/main.tf
@@ -5,7 +5,7 @@ terraform {
 }
 
 data "aws_vpc" "development" {
-  cidr_block = "10.51.0.0/21"
+  cidr_block = var.cidr_v4
 }
 data "aws_subnet_ids" "private" {
   vpc_id = data.aws_vpc.development.id
@@ -13,4 +13,21 @@ data "aws_subnet_ids" "private" {
     name   = "tag:Name"
     values = ["development-subnet-private"]
   }
+}
+
+data "aws_subnet_ids" "public" {
+  vpc_id = data.aws_vpc.development.id
+  filter {
+    name   = "tag:Name"
+    values = ["development-subnet-public"]
+  }
+}
+
+data "aws_subnet" "cidr_public" {
+  for_each = data.aws_subnet_ids.public.ids
+  id       = each.value
+}
+data "aws_subnet" "cidr_private" {
+  for_each = data.aws_subnet_ids.private.ids
+  id       = each.value
 }
