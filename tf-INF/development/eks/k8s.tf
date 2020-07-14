750099e183ee49fecec74140fbe0836b38c228cb
phuong-leeo <p.le@ryte.com>
2020-07-14 17:11:52 +0700

    refactor code

--- tf-INF/development/eks/k8s.tf
+++ tf-INF/development/eks/k8s.tf
@@ -1,7 +1,7 @@
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.production.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.production.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.production.token
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.eks.token
   load_config_file       = false
   version                = "~> 1.9"
 }
