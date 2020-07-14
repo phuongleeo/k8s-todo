3d66ebb306f055af1be99c4f9e25aa94a6fbbe16
phuong-leeo <p.le@ryte.com>
2020-07-14 17:11:52 +0700

    refactor code

--- tf-INF/helpers/import_symlink.sh
+++ tf-INF/helpers/import_symlink.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in config.tf locals.tf terraform.tfvars variables.tf versions.tf
+for i in config.tf locals.tf terraform.tfvars variables.tf versions.tf config_helm.tf
 do
  /bin/ln -sf ../$i .
 done
