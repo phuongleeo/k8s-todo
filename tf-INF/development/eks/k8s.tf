50fe8b11f4e558cf2f5f7ec4820c803d1f60ffc1
phuong-leeo <p.le@ryte.com>
2020-07-28 22:44:48 +0700

    fixed serviceaccount role, external-dns, cluster-autoscaler is now worked

--- tf-INF/development/eks/k8s.tf
+++ tf-INF/development/eks/k8s.tf
@@ -72,124 +72,124 @@ resource "kubernetes_cluster_role_binding" "admin" {
   }
 }
 
-//external dns
-resource "kubernetes_cluster_role" "external_dns" {
-  metadata {
-    name = "external-dns-role"
-    annotations = {
-      "rbac.authorization.kubernetes.io/autoupdate" = true
-    }
-  }
-
-  rule {
-    api_groups = ["*"]
-    resources  = ["services", "endpoints", "pods"]
-    verbs      = ["get", "watch", "list"]
-  }
-  rule {
-    api_groups = ["extensions"]
-    resources  = ["ingresses"]
-    verbs      = ["get", "watch", "list"]
-  }
-  rule {
-    api_groups = ["*"]
-    resources  = ["nodes"]
-    verbs      = ["watch", "list"]
-  }
-}
-
-resource "kubernetes_service_account" "external_dns" {
-  metadata {
-    name = "external-dns"
-    # namespace = kubernetes_namespace.bootstrap.metadata.0.name
-    annotations = {
-      "eks.amazonaws.com/role-arn" = aws_iam_role.external_dns.arn
-    }
-  }
-  secret {
-    name = kubernetes_secret.external_dns.metadata.0.name
-  }
-}
-
-resource "kubernetes_secret" "external_dns" {
-  metadata {
-    name = "external-dns"
-  }
-}
-resource "kubernetes_cluster_role_binding" "external_dns" {
-  metadata {
-    name = "external-dns"
-    # namespace = kubernetes_namespace.bootstrap.metadata.0.name
-  }
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "ClusterRole"
-    name      = kubernetes_cluster_role.external_dns.metadata.0.name
-  }
-  subject {
-    kind      = "ServiceAccount"
-    name      = kubernetes_service_account.external_dns.metadata.0.name
-    namespace = " "
-  }
-}
-
-//gohabor
-resource "kubernetes_cluster_role" "harbor" {
-  metadata {
-    name = "harbor-role"
-    annotations = {
-      "rbac.authorization.kubernetes.io/autoupdate" = true
-    }
-  }
-
-  rule {
-    api_groups = ["*"]
-    resources  = ["services", "endpoints", "pods"]
-    verbs      = ["get", "watch", "list"]
-  }
-  rule {
-    api_groups = ["extensions"]
-    resources  = ["ingresses"]
-    verbs      = ["get", "watch", "list"]
-  }
-  rule {
-    api_groups = ["*"]
-    resources  = ["nodes"]
-    verbs      = ["watch", "list"]
-  }
-}
-
-resource "kubernetes_service_account" "harbor" {
-  metadata {
-    name = "harbor"
-    # namespace = kubernetes_namespace.bootstrap.metadata.0.name
-    annotations = {
-      "eks.amazonaws.com/role-arn" = aws_iam_role.harbor.arn
-    }
-  }
-  secret {
-    name = kubernetes_secret.harbor.metadata.0.name
-  }
-}
-
-resource "kubernetes_secret" "harbor" {
-  metadata {
-    name = "harbor"
-  }
-}
-resource "kubernetes_cluster_role_binding" "harbor" {
-  metadata {
-    name = "harbor"
-    # namespace = kubernetes_namespace.bootstrap.metadata.0.name
-  }
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "ClusterRole"
-    name      = kubernetes_cluster_role.harbor.metadata.0.name
-  }
-  subject {
-    kind      = "ServiceAccount"
-    name      = kubernetes_service_account.harbor.metadata.0.name
-    namespace = " "
-  }
-}
+# //external dns
+# resource "kubernetes_cluster_role" "external_dns" {
+#   metadata {
+#     name = "external-dns-role"
+#     annotations = {
+#       "rbac.authorization.kubernetes.io/autoupdate" = true
+#     }
+#   }
+#
+#   rule {
+#     api_groups = ["*"]
+#     resources  = ["services", "endpoints", "pods"]
+#     verbs      = ["get", "watch", "list"]
+#   }
+#   rule {
+#     api_groups = ["extensions"]
+#     resources  = ["ingresses"]
+#     verbs      = ["get", "watch", "list"]
+#   }
+#   rule {
+#     api_groups = ["*"]
+#     resources  = ["nodes"]
+#     verbs      = ["watch", "list"]
+#   }
+# }
+#
+# resource "kubernetes_service_account" "external_dns" {
+#   metadata {
+#     name = "external-dns"
+#     namespace = kubernetes_namespace.bootstrap.metadata.0.name
+#     annotations = {
+#       "eks.amazonaws.com/role-arn" = module.external_dns_role.arn
+#     }
+#   }
+#   secret {
+#     name = kubernetes_secret.external_dns.metadata.0.name
+#   }
+# }
+#
+# resource "kubernetes_secret" "external_dns" {
+#   metadata {
+#     name = "external-dns"
+#   }
+# }
+# resource "kubernetes_cluster_role_binding" "external_dns" {
+#   metadata {
+#     name = "external-dns"
+#     # namespace = kubernetes_namespace.bootstrap.metadata.0.name
+#   }
+#   role_ref {
+#     api_group = "rbac.authorization.k8s.io"
+#     kind      = "ClusterRole"
+#     name      = kubernetes_cluster_role.external_dns.metadata.0.name
+#   }
+#   subject {
+#     kind      = "ServiceAccount"
+#     name      = kubernetes_service_account.external_dns.metadata.0.name
+#     namespace = " "
+#   }
+# }
+#
+# //gohabor
+# resource "kubernetes_cluster_role" "harbor" {
+#   metadata {
+#     name = "harbor-role"
+#     annotations = {
+#       "rbac.authorization.kubernetes.io/autoupdate" = true
+#     }
+#   }
+#
+#   rule {
+#     api_groups = ["*"]
+#     resources  = ["services", "endpoints", "pods"]
+#     verbs      = ["get", "watch", "list"]
+#   }
+#   rule {
+#     api_groups = ["extensions"]
+#     resources  = ["ingresses"]
+#     verbs      = ["get", "watch", "list"]
+#   }
+#   rule {
+#     api_groups = ["*"]
+#     resources  = ["nodes"]
+#     verbs      = ["watch", "list"]
+#   }
+# }
+#
+# resource "kubernetes_service_account" "harbor" {
+#   metadata {
+#     name = "harbor"
+#     # namespace = kubernetes_namespace.bootstrap.metadata.0.name
+#     annotations = {
+#       "eks.amazonaws.com/role-arn" = aws_iam_role.harbor.arn
+#     }
+#   }
+#   secret {
+#     name = kubernetes_secret.harbor.metadata.0.name
+#   }
+# }
+#
+# resource "kubernetes_secret" "harbor" {
+#   metadata {
+#     name = "harbor"
+#   }
+# }
+# resource "kubernetes_cluster_role_binding" "harbor" {
+#   metadata {
+#     name = "harbor"
+#     # namespace = kubernetes_namespace.bootstrap.metadata.0.name
+#   }
+#   role_ref {
+#     api_group = "rbac.authorization.k8s.io"
+#     kind      = "ClusterRole"
+#     name      = kubernetes_cluster_role.harbor.metadata.0.name
+#   }
+#   subject {
+#     kind      = "ServiceAccount"
+#     name      = kubernetes_service_account.harbor.metadata.0.name
+#     namespace = " "
+#   }
+# }
