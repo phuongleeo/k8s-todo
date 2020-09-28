770555140b040738187bf0b5497354d26d6e0179
phuongleeo <phuongley@gmail.com>
2020-09-28 14:31:36 +0700

    add VPC service

--- tf-INF/development/eks/main.tf
+++ tf-INF/development/eks/main.tf
@@ -4,33 +4,6 @@ terraform {
   }
 }
 
-data "aws_vpc" "development" {
-  cidr_block = var.cidr_v4
-}
-data "aws_subnet_ids" "private" {
-  vpc_id = data.aws_vpc.development.id
-  filter {
-    name   = "tag:Name"
-    values = ["development-subnet-private"]
-  }
-}
-
-data "aws_subnet_ids" "public" {
-  vpc_id = data.aws_vpc.development.id
-  filter {
-    name   = "tag:Name"
-    values = ["development-subnet-public"]
-  }
-}
-
-data "aws_subnet" "cidr_public" {
-  for_each = data.aws_subnet_ids.public.ids
-  id       = each.value
-}
-data "aws_subnet" "cidr_private" {
-  for_each = data.aws_subnet_ids.private.ids
-  id       = each.value
-}
 
 data "terraform_remote_state" "s3" {
   backend = "s3"
@@ -72,6 +45,17 @@ data "terraform_remote_state" "iam" {
   config = {
     bucket = "${var.remote_state_bucket}"
     key    = "stacks/iam"
+    region = "${var.aws_region}"
+  }
+
+  workspace = "${terraform.workspace}"
+}
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config = {
+    bucket = "${var.remote_state_bucket}"
+    key    = "stacks/vpc"
     region = "${var.aws_region}"
   }
 
