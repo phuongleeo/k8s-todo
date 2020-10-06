6c3caa5f9ca75eba54d7b909a8dce179c75a5e75
phuongleeo <phuongley@gmail.com>
2020-10-06 23:06:41 +0700

    - Fix harbor registry patch file - Update k8s-providers

--- tf-INF/development/eks/k8s.tf
+++ tf-INF/development/eks/k8s.tf
@@ -1,10 +1,3 @@
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.eks.token
-  load_config_file       = false
-  version                = "~> 1.10"
-}
 //namespace
 resource "kubernetes_namespace" "bootstrap" {
   metadata {
