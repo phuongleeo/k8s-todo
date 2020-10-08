f33dd57eeab8f1fffd5b8c53e1b40f0f7e2b90c2
Alex Co <alex@uiza.io>
2020-10-08 13:25:11 +0700

    Replace symlinks with local files for state module

--- tf-INF/development/state/locals.tf
+++ tf-INF/development/state/locals.tf
@@ -1,1 +1,9 @@
-../locals.tf
+locals {
+  common_tags = {
+    "Project"     = var.project
+    "Environment" = var.environment
+    "Provisioner" = "Terraform"
+  }
+
+  eks_domain = var.domain_name
+}
--- tf-INF/development/state/variables.tf
+++ tf-INF/development/state/variables.tf
@@ -1,1 +1,15 @@
-../variables.tf
+// taken from terraform.tfvars
+
+variable "aws_account" {}
+
+variable "aws_region" {}
+
+variable "remote_state_bucket" {}
+
+variable "environment" {}
+
+variable "domain_env" {}
+
+variable "project" {}
+
+variable "domain_name" {}
