66427dd3c7e9ed7914ba2c1e483ba194b44740db
Alex Co <alex@uiza.io>
2020-10-08 14:31:05 +0700

    Remove obsoleted files

--- tf-INF/development/config.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  allowed_account_ids = [var.aws_account]
-
-  region  = var.aws_region
-  version = "~> 2.40"
-}
-
-data "aws_region" "current" {
-}
--- tf-INF/development/variables.tf
+++
@@ -1,7 +0,0 @@
-// taken from terraform.tfvars
-
-variable "aws_account" {}
-
-variable "aws_region" {}
-
-variable "remote_state_bucket" {}
